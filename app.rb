a0678df30e2f1f7a66bd4e4d45d8e8d8e78ee477
Clayton Easterbrooks & Shannon Mulloy <student@epicodus.com>
2015-08-12 16:02:46 -0700

    working prototype

---
+++ app.rb
@@ -0,0 +1,21 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/hangman')
+
+get('/') do
+  erb(:index)
+end
+
+post('/guess') do
+  guess = params.fetch('guess')
+  @@masked_word = @@word.check(guess)
+  erb(:index)
+end
+
+post('/play_word') do
+  play_word = params.fetch('play_word')
+  @@word = Word.new(play_word)
+  @@masked_word = @@word.masked_word()
+  erb(:index)
+end
